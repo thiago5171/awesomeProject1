7ee9e01f4d2f943ad7986556402661da7782847e
thiago5171 <Tgazaroli@gmail.com>
2021-04-02 18:35:39 -0300

    Update Contato.go

--- estruturasdedados/Contato.go
+++ estruturasdedados/Contato.go
@@ -11,8 +11,8 @@ type Contato struct {
 }
 
 // metodo
-func (c Contato) VerContato() {
-
+func (c Contato) VerContato() {// o c  vai pegar a o objeto  e introduzir na func e o nome ao lado é o seu tipo
+						//|->nome do metodo 
 	fmt.Println("Nome->", c.Nome)
 	fmt.Println("Telefone->", c.Telefone)
 	fmt.Println("Email->", c.Email)
@@ -21,7 +21,7 @@ func (c Contato) VerContato() {
 // metodo
 func (this Contato) Equal(contat Contato) bool {
 	if this.Email == contat.Email {
-		return true
+		return true// esse return caso seja tivado impede que o de bai
 	}
 
 	return false
